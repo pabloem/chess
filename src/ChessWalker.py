4207940b9e5317920e022c80394b25ad99807e64
Pablo <polecito.em@gmail.com>
2015-09-21 11:42:35 +0900

    Food chain calculator is up

---
+++ src/ChessWalker.py
@@ -0,0 +1,115 @@
+import chess
+from chess import pgn
+
+"""
+ChessWalker
+- Piece notation:
+-- Pieces are reported with two or three characters. One for color, and one
+-- for piece type. The piece is always reported in uppercase letters.
+-- The piece color is either 'W' or 'B'. The piece types are standard
+-- "P" for Pawn, "R" for Rook, "N" for Knight, "B" for Bishop, "Q" for
+-- Queen, and "K" for King.
+-- Aditionally, Pawns have their row position appended at the end (e.g. "WP2")
+
+Examples are:
+-- 'WP2' - white pawn on its starting position
+-- 'BR' - black rook
+-- 'WQ' - white queen
+-- 'BP2' - black pawn about to crown itself
+
+- Move notation:
+-- Move types are reported either as 'T' or 'NT', meaning a 'taking move'
+-- (a move where a piece is taken), or a 'non-taking move' (a move where
+-- no pieces are taken).
+"""
+class ChessWalker:
+    def __init__(self,game_file):
+        self._f = game_file
+        self._fp = open(game_file)
+
+    # Feteches the next game frm the pgn input file
+    # Returns False if this is the last 
+    def fetch_game(self):
+        self._g = pgn.read_game(self._fp)
+        return self._g is not None
+
+    def close(self):
+        self._fp.close()
+
+    # Moves the state of the game to the next movement
+    # Returns False if it's the end of the game. True otherwise.
+    def next(self):
+        if len(self._g.variations) > 0:
+            self._g = self._g.variation(0)
+            return True
+        return False
+
+    # Returns the algebraic notation of the following move
+    # Returns False if there is no next move
+    def get_move_notation(self):
+        g = self._g
+        if len(g.variations) == 0:
+            return False
+        b = g.board()
+        notation = b.san(g.variation(0).move)
+        return notation
+    
+    # Returns "T" if move is a take, "NT" if not.
+    # Returns False if this is the last position
+    def get_move_type(self):
+        g = self._g
+        if len(g.variations) == 0: return False
+        b = g.board()
+        m = g.variation(0).move
+        return 'T' if b.is_capture(m) else 'NT'
+
+    def get_piece_notation(self,square):
+        g = self._g
+        piece = g.board().piece_at(square)
+        if piece is None: return None
+        symbol = piece.symbol()
+        if symbol in ['P','p']:
+            symbol = symbol+str(square//8+1)
+        if symbol.upper() == symbol:
+            return "W"+symbol
+        return "B"+symbol.upper()
+    
+    # Returns False if this is the last position
+    # Returns False if the move is not a taking move
+    # Returns the symbol of the taker piece otherwise
+    def get_move_taker(self):
+        if self.get_move_type() in ['NT', False]:
+            return False
+        g = self._g
+        t_square = g.variation(0).move.from_square
+        return self.get_piece_notation(t_square)
+
+    # Returns False if this is the last position
+    # Returns False if the move is not a taking move
+    # Returns the symbol of the taker piece otherwise
+    def get_move_taken(self):
+        if self.get_move_type() in ['NT', False]:
+            return False
+        g = self._g
+        b = g.board()
+        m = g.variation(0).move
+        
+        if b.is_en_passant(m): # Special case for capture en passant
+            sq = m.from_square - (m.from_square % 8 - m.to_square % 8)
+            return self.get_piece_notation(sq)
+        
+        t_square = m.to_square
+        return self.get_piece_notation(t_square)
+
+    # Moves the state to the next TAKING move of the game, or
+    # the last one
+    def next_taker(self):
+        g = self._g
+        while self.next():
+            mv_type = self.get_move_type()
+            if mv_type == 'T':
+                return True
+            if mv_type == 'NT':
+                continue
+            break
+        return False
---
+++ src/FoodChainBuilder.py
@@ -0,0 +1,68 @@
+from ChessWalker import ChessWalker
+class FoodChainBuilder:
+    def __init__(self,in_file):
+        self._cw = ChessWalker(in_file)
+        self._sets = []
+        self._pairs = []
+
+    def setup(self):
+        self.T = [[], # Start takers
+                  []] # End takers
+        self.t = [[], # Start taken
+                  []] # End taken
+
+    def append_sets(self):
+        if len(self.t[1]) > 0:
+            self._sets.append({'taker':self.T[0],'taken':self.t[1]})
+        if len(self.t[0]) > 0:
+            self._sets.append({'taker':self.T[1],'taken':self.t[0]})
+
+    def append_pair(self,taker,taken):
+        self._pairs.append([taker,taken])
+
+    def _switch(self):
+        tmp = self.T[0]
+        self.T[0] = self.T[1]
+        self.T[1] = tmp
+
+        tmp = self.t[0]
+        self.t[0] = self.t[1]
+        self.t[1] = tmp
+        return
+
+    def flush_sets(self,callback):
+        if callback is not None: callback(self._sets)
+        print("Flushed dem sets")
+        self._sets = []
+
+    def flush_pairs(self,callback):
+        if callback is not None: callback(self._pairs)
+        self._pairs = []
+        
+    def go(self,flush_pairs,flush_sets):
+        cw = self._cw
+        while cw.fetch_game():
+            # We assume that the first move is not a taking move - this is reasonable for
+            # normal chess games.
+            while cw.next_taker():
+                self.setup()
+                self.T[0].append(cw.get_move_taker())
+                self.t[1].append(cw.get_move_taken())
+                self.T[1].append(cw.get_move_taken())
+                self.append_pair(cw.get_move_taker(),cw.get_move_taken())
+                # Some functions some functions
+                while cw.next():
+                    if cw.get_move_type() == 'NT':
+                        break
+                    self._switch()
+                    self.T[0].append(cw.get_move_taker())
+                    self.t[1].append(cw.get_move_taken())
+                    #self.T[1].append(cw.get_move_taken())
+                    self.append_pair(cw.get_move_taker(),cw.get_move_taken())                    
+                # Conclude
+                self.append_sets()
+            self.flush_sets(flush_sets)
+            self.flush_pairs(flush_pairs)
+        cw.close()
+        return
+
